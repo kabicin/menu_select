b107429f4c1f45a50887471c2e014a85b64d07be
kabicin <kirbychin99@gmail.com>
2019-02-21 23:28:03 -0500

    Added administrator login

--- android_v1.0/MenuSelect/app/src/main/java/dynastyhouse/menuselect/MainActivity.java
+++ android_v1.0/MenuSelect/app/src/main/java/dynastyhouse/menuselect/MainActivity.java
@@ -1,13 +1,47 @@
 package dynastyhouse.menuselect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
+    }
+
+    /**
+     * onClick listener for the login button
+     *
+     * @param v The current view
+     */
+    public void tryLogin(View v) {
+        if (validateLogin()) {
+            Toast.makeText(this, "Successfully logged in!", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this, ResourceCentreActivity.class);
+            startActivity(i);
+        } else {
+            Toast.makeText(this, "Invalid username/password.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Validates login information with admin login credentials (for debugging)
+     *
+     * @return true if the user credentials are correct, otherwise false.
+     */
+    private boolean validateLogin() {
+        if (username == null || password == null)
+            return false;
+        return username.getText().toString().equals("admin") && password.getText().toString().equals("123");
     }
 }
